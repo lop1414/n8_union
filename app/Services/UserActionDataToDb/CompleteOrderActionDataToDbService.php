1900c42ecb641e94612a6903891cb2f3d42dbe26
LinYouXin <310467466@qq.com>
2021-04-09 18:12:21 +0800

    -

--- app/Services/UserActionDataToDb/CompleteOrderActionDataToDbService.php
+++ app/Services/UserActionDataToDb/CompleteOrderActionDataToDbService.php
@@ -10,7 +10,7 @@ use App\Enums\QueueEnums;
 use App\Models\OrderModel;
 
 
-class CompleteOrderActionDataToDBService extends UserActionDataToDbService
+class CompleteOrderActionDataToDbService extends UserActionDataToDbService
 {
 
     protected $queueEnum = QueueEnums::COMPLETE_ORDER;
