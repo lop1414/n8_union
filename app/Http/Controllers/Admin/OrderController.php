9fc9a47d419e6993c838f9a0c27899a4bc7ca061
LinYouXin <310467466@qq.com>
2021-04-10 17:46:48 +0800

    -

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -59,11 +59,6 @@ class OrderController extends UserActionBaseController
 
         $this->selectCommonPrepare(ConvertTypeEnum::ORDER);
         $this->selectCommonPrepare(ConvertTypeEnum::PAY);
-        $this->curdService->selectQueryAfter(function() {
-            foreach ($this->curdService->responseData['list'] as $item){
-                $item->amount /=  100;
-            }
-        });
 
     }
 
