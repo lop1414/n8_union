c87d7f76622b6200acdd3a133723ea8ac6bc5e3a
LinYouXin <310467466@qq.com>
2021-06-23 10:22:40 +0800

    -

--- app/Http/Controllers/Admin/ChannelController.php
+++ app/Http/Controllers/Admin/ChannelController.php
@@ -73,7 +73,7 @@ class ChannelController extends BaseController
             }
 
             $req = $this->curdService->requestData;
-            if(isset($req['is_bind']) && $req['is_bind'] == 1){
+            if(isset($req['is_bind']) && $req['is_bind'] == 0){
                 $builder->whereNull('e.admin_id');
             }else{
                 $builder->where('e.admin_id','>',0);
