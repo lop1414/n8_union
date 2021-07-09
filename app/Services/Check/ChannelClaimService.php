b74badf2b3bde2c774263a636d00cd38f10cbc67
LinYouXin <310467466@qq.com>
2021-07-09 15:32:54 +0800

    渠道认领提醒调整

--- app/Services/Check/ChannelClaimService.php
+++ app/Services/Check/ChannelClaimService.php
@@ -3,62 +3,60 @@
 namespace App\Services\Check;
 
 
-
-
-use App\Common\Enums\CpTypeEnums;
+use App\Common\Enums\AdvAliasEnum;
 use App\Common\Enums\StatusEnum;
-use App\Datas\ProductData;
-use App\Models\ChannelModel;
-use Illuminate\Support\Facades\DB;
+use App\Common\Services\SystemApi\AdvOceanApiService;
+use App\Datas\ChannelData;
+use App\Models\ChannelExtendModel;
 
 class ChannelClaimService extends CheckService
 {
 
 
-    public $sendTitle = '请及时认领渠道';
+    public $sendTitle = '渠道认领提示';
 
 
     public function index(){
 
-        $halfHourAgo = date('Y-m-d H:i:s',time() - 60 *30);
-        $unclaimedChannelProduct =  (new ChannelModel())
-            ->leftJoin('channel_extends AS e','channels.id','=','e.channel_id')
-            ->select(DB::raw('channels.*'))
-            ->whereNull('e.admin_id')
-            ->where('channels.create_time','<=',$halfHourAgo)
-            ->get();
+        $startTime = date('Y-m-d H:i:s',time() - 60 * 60 * 24);
+        $endTime = date('Y-m-d H:i:s');
 
-        $this->setMarketAllAdmin();
+        $time = $startTime;
+        while($time < $endTime){
+            $tmpEndTime = date('Y-m-d H:i:s',  strtotime($time) + 60 * 60 *2);
 
-        $content = [];
-        foreach ($unclaimedChannelProduct as $channel){
 
-            if($this->isNeedSend($this->getKey($channel['id']))){
-                $content[$channel['product_id']][] = $channel->toArray();
-            }
-        }
+            $list = (new AdvOceanApiService())->apiGetChannelAds($time,$tmpEndTime);
 
-        foreach ($content as $productId => $item){
-            $product = (new ProductData())->setParams(['id' => $productId])->read();
+            $advNameMap = array_column(AdvAliasEnum::$list,'name','id');
+            $statusNameMap = array_column(StatusEnum::$list,'name','id');
 
-            // 只检测阅文平台
-            if($product['cp_type'] != CpTypeEnums::YW || $product['status'] != StatusEnum::ENABLE) continue;
-
-            $tmpItem = array_chunk($item,'5');
-
-            foreach ($tmpItem as $sendItem){
-                $tmp = "产品：{$product['name']}<br>";
-                $tmp .= "渠道：<br>";
-                foreach ($sendItem as $c){
-                    $tmp .= "          {$c['name']}<br>";
-                    $this->recordSendLog($this->getKey($c['id']));
+            foreach ($list as  $item){
+                $channelExtend = (new ChannelExtendModel())->where('channel_id',$item['channel_id'])->first();
+                if(!empty($channelExtend)){
+                    continue;
                 }
+
+                $channelExtendModel = new ChannelExtendModel();
+                $channelExtendModel->channel_id = $item['channel_id'];
+                $channelExtendModel->adv_alias = AdvAliasEnum::OCEAN;
+                $channelExtendModel->status = StatusEnum::ENABLE;
+                $channelExtendModel->admin_id = $item['admin_id'];
+                $channelExtendModel->save();
+
+
+                $channel = (new ChannelData())->setParams(['id'=>$item['channel_id']])->read();
+
+                $tmp = "检测到渠道 {$channel['name']} 正在投放，已默认认领<br>";
+                $tmp .= "广告商：".$advNameMap[$channelExtendModel->adv_alias]."<br>";
+                $tmp .= "状态：".$statusNameMap[$channelExtendModel->status]."<br>";
+                $this->sendAdminIds=[25,$channelExtendModel->admin_id];
                 $this->sendContent = $tmp;
                 $this->sendMessage();
             }
 
+            $time = $tmpEndTime;
         }
-
     }
 
 
