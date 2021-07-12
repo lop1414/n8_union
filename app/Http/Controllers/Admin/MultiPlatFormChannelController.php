f5ee9e8e020043e43655f8edf77c0bde8b9a217e
LinYouXin <310467466@qq.com>
2021-07-12 18:53:20 +0800

    -

--- app/Http/Controllers/Admin/MultiPlatFormChannelController.php
+++ app/Http/Controllers/Admin/MultiPlatFormChannelController.php
@@ -192,13 +192,13 @@ class MultiPlatFormChannelController extends BaseController
         $this->curdService->saveBefore(function(){
             //验证
             $androidChannel = (new ChannelExtendData())
-                ->setParams(['id' => $this->curdService->handleData['android_channel_id']])
+                ->setParams(['channel_id' => $this->curdService->handleData['android_channel_id']])
                 ->read();
             $androidProduct = (new ProductData())
                 ->setParams(['id' => $androidChannel['product_id']])
                 ->read();
             $iosChannel = (new ChannelExtendData())
-                ->setParams(['id' => $this->curdService->handleData['ios_channel_id']])
+                ->setParams(['channel_id' => $this->curdService->handleData['ios_channel_id']])
                 ->read();
             $iosProduct = (new ProductData())
                 ->setParams(['id' => $iosChannel['product_id']])
