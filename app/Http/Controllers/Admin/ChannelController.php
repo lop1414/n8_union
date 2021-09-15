05bb37e7df51684d81251357a0fa0246c3152179
LinYouXin <310467466@qq.com>
2021-09-15 11:32:16 +0800

    积木鱼跳转链接

--- app/Http/Controllers/Admin/ChannelController.php
+++ app/Http/Controllers/Admin/ChannelController.php
@@ -136,7 +136,8 @@ class ChannelController extends BaseController
                 $item->admin_name = $item->admin_id ? $map[$item->admin_id]['name'] : '';
                 $item->has_extend = $item->admin_id ? true : false;
                 if($item->product->type == ProductTypeEnums::H5 && $item['adv_alias'] == AdvAliasEnum::BD ){
-                    $item->jmy_forward_url =  str_replace('__CHANNEL_ID__',$item['id'],$jmyForwardUrl);
+                    $indexPageUrl = $item->product->extends->index_page_url ?? '';
+                    $item->jmy_forward_url = str_replace('__CHANNEL_ID__',$item['id'],$jmyForwardUrl). '&url='.$indexPageUrl;
                 }
             }
         });
