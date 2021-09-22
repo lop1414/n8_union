2d54d32793fde8ecd7310e8de049ced6d962e102
LinYouXin <310467466@qq.com>
2021-09-22 15:56:32 +0800

    修复用户广告商信息

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use App\Common\Controllers\Front\FrontController;
 
 
+use App\Common\Enums\AdvAliasEnum;
+use App\Datas\N8UnionUserData;
+use App\Models\N8UnionUserModel;
 use App\Services\SaveUserAction\SaveFollowActionService;
 use App\Services\SaveUserAction\SaveReadActionService;
 use App\Services\SaveUserAction\SaveRegActionService;
@@ -29,10 +32,23 @@ class TestController extends FrontController
         if($key != 'aut'){
             return $this->forbidden();
         }
+        $this->demo();
 
-         (new SaveFollowActionService())->run();
-         (new SaveRegActionService())->run();
+    }
 
+    public function demo(){
+        $list = (new N8UnionUserModel())
+            ->whereBetween('created_time',['2021-09-19 00:00:00','2021-09-23 00:00:00'])
+            ->where('channel_id','53374')
+            ->get();
+        $modelData = new N8UnionUserData();
+        foreach ($list as $item){
+            $modelData->update(['id' => $item->id],[
+                'adv_alias' => AdvAliasEnum::BD,
+                'click_id'  => 0
+            ]);
+            echo $item->id. "\n";
+        }
 
     }
 
