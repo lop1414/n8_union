57beae77bad40f6bbf476352724e50c67352a78b
linyouxin <yllobx@outlook.com>
2021-11-17 16:17:21 +0800

    -

--- app/Services/ChannelService.php
+++ app/Services/ChannelService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\Common\Enums\CpTypeEnums;
 use App\Common\Services\BaseService;
 use App\Common\Tools\CustomException;
 use App\Datas\ChannelData;
@@ -9,6 +10,45 @@ use App\Datas\ChannelExtendData;
 
 class ChannelService extends BaseService
 {
+
+    protected $cpChannelServices = [
+        CpTypeEnums::BM => [
+            'name' => '笔墨',
+            'class' => \App\Services\Cp\Channel\BmChannelService::class
+        ],
+        CpTypeEnums::TW =>[
+            'name' => '腾文',
+            'class' => \App\Services\Cp\Channel\TwChannelService::class
+        ],
+        CpTypeEnums::QY =>[
+            'name' => '七悦',
+            'class' => \App\Services\Cp\Channel\QyChannelService::class
+        ],
+        CpTypeEnums::YW =>[
+            'name' => '阅文',
+            'class' => \App\Services\Cp\Channel\YwChannelService::class
+        ]
+    ];
+
+
+    /**
+     * @param null $cpType
+     * @return array|string[]
+     */
+    public function getCpService($cpType = null){
+
+        if(empty($this->cpChannelServices[$cpType])) return [''];
+
+        return $this->cpChannelServices[$cpType];
+    }
+
+    /**
+     * @return string[][]
+     */
+    public function getAllCpServices(){
+        return $this->cpChannelServices;
+    }
+
 
     /**
      * @param $productId
