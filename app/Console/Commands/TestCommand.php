e3e7e0ef5a09788e6f742c5c24400acca71175ec
linyouxin <yllobx@outlook.com>
2022-03-10 15:31:21 +0800

    -

--- app/Console/Commands/TestCommand.php
+++ app/Console/Commands/TestCommand.php
@@ -3,19 +3,10 @@
 namespace App\Console\Commands;
 
 use App\Common\Console\BaseCommand;
-use App\Common\Enums\AdvAliasEnum;
-use App\Common\Enums\OrderStatusEnums;
-use App\Common\Helpers\Functions;
 use App\Common\Services\ConsoleEchoService;
-use App\Datas\ChannelData;
-use App\Datas\ChannelExtendData;
-use App\Datas\N8UnionUserData;
-use App\Enums\N8UserTypeEnum;
-use App\Models\N8UnionUserModel;
-use App\Models\OrderModel;
-use App\Models\UserReadActionModel;
-use App\Services\N8UnionUserService;
-use App\Services\UserBookReadService;
+use App\Models\DeviceModel;
+use App\Services\DeviceNetworkLicenseService;
+
 
 class TestCommand extends BaseCommand
 {
@@ -47,95 +38,15 @@ class TestCommand extends BaseCommand
 
 
     public function handle(){
-        $this->updateN8UnionUserType();
-    }
-
-
-    // 更新n8_union_user user_type
-    public function updateN8UnionUserType(){
-        $lastId = 0;
-        do{
-            $list = (new N8UnionUserModel())
-                ->where('id','>',$lastId)
-                ->where('user_type','=','')
-                ->skip(0)
-                ->take(1000)
-                ->orderBy('id')
-                ->get();
-            foreach ($list as $item){
-                $lastId = $item->id;
-                $n8UserSum =  (new N8UnionUserModel())
-                    ->where('n8_guid',$item['n8_guid'])
-                    ->where('created_time','<',$item['created_time'])
-                    ->count();
-                $item->user_type = $n8UserSum > 0 ? N8UserTypeEnum::BACKFLOW : N8UserTypeEnum::NEW;
-                $item->save();
-                // 删缓存
-                (new N8UnionUserData())->setParams(['id' => $item->id])->clear();
-                echo "\r".$lastId;
-            }
-
-        }while(!$list->isEmpty());
-    }
-
-
-    public function userBookRead(){
-        $tableList = ['user_read_actions_202109'];
-        $services = new UserBookReadService();
-        foreach ($tableList as $tableName){
-            echo $tableName ."\n\n\n\n";
-            $lastId = 4808788;
-            do{
-                $list = (new UserReadActionModel())
-                    ->setTable($tableName)
-                    ->where('id','>',$lastId)
-                    ->skip(0)
-                    ->take(1000)
-                    ->orderBy('id')
-                    ->get();
-                foreach ($list as $item){
-                    $lastId = $item->id;
-                    echo "\r".$lastId;
-                    $services->analysis($item);
-                }
-
-            }while(!$list->isEmpty());
-        }
-    }
-
-
-
-
-    // 更新订单下单次数 完成次数
-    public function updateOrderTimes(){
-        $startTime = '2021-07-01 00:00:00';
-        $endTime = '2021-07-02 00:00:00';
-
-        $list = (new OrderModel())
-            ->whereBetween('order_time',[$startTime,$endTime])
-            ->get();
+        $service = new DeviceNetworkLicenseService();
+        $list = (new DeviceModel())->where('brand','')->list();
         foreach ($list as $item){
-            $orderTimes = (new OrderModel())
-                ->where('n8_guid',$item['n8_guid'])
-                ->where('channel_id',$item['channel_id'])
-                ->where('order_time','<',$item['order_time'])
-                ->count();
-
-            $completeTimes = (new OrderModel())
-                ->where('n8_guid',$item['n8_guid'])
-                ->where('channel_id',$item['channel_id'])
-                ->where('status',OrderStatusEnums::COMPLETE)
-                ->where('order_time','<',$item['order_time'])
-                ->count();
-
-            $item->order_times = $orderTimes + 1;
-            $item->complete_times = $item['status'] == OrderStatusEnums::COMPLETE ? $completeTimes + 1 : $completeTimes;
+            $item->brand = $service->getBrand($item->model);
             $item->save();
+            echo $item->brand."\n";
         }
-
-
-
     }
+
 
 
 }
