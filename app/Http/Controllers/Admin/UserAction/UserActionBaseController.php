876be0337c924db479fecff79578d5231c21aaf3
linyouxin <yllobx@outlook.com>
2022-03-31 16:47:08 +0800

    -

--- app/Http/Controllers/Admin/UserAction/UserActionBaseController.php
+++ app/Http/Controllers/Admin/UserAction/UserActionBaseController.php
@@ -29,28 +29,27 @@ class UserActionBaseController extends BaseController
                 $requestData = $this->curdService->requestData;
                 $unionUserQuery = DB::table('n8_union_users');
 
-                $isSelf = $requestData['is_self'] ?? 1;
-                $isSelf && $unionUserQuery->where('admin_id', $this->adminUserService->readId());
+                $unionUser = $unionUserQuery->select('id');
+                $builder->LeftjoinSub($unionUser, 'union_user', function ($join) use ($unionUserId) {
+                    $tableName = $this->model->getTable();
+                    $join->on($tableName . '.' . $unionUserId, '=', 'union_user.id');
+                });
 
-                !empty($requestData['admin_id']) && $unionUserQuery->where('admin_id', $requestData['admin_id']);
+                $isSelf = $requestData['is_self'] ?? 1;
+                $isSelf && $builder->where('union_user.admin_id', $this->adminUserService->readId());
+
+                !empty($requestData['admin_id']) && $builder->where('union_user.admin_id', $requestData['admin_id']);
 
                 if (!$this->adminUserService->isAdmin()) {
                     $adminIds = $this->adminUserService->getChildrenAdminIds();
-                    $unionUserQuery->whereIn('admin_id', $adminIds);
+                    $builder->whereIn('union_user.admin_id', $adminIds);
                 }
 
-                !empty($requestData['channel_id']) && $unionUserQuery->where('channel_id', $requestData['channel_id']);
-                !empty($requestData['user_type']) && $unionUserQuery->where('user_type', $requestData['user_type']);
-                !empty($requestData['created_time']) && $unionUserQuery->whereBetween('created_time', $requestData['created_time']);
+                !empty($requestData['channel_id']) && $builder->where('union_user.channel_id', $requestData['channel_id']);
+                !empty($requestData['user_type']) && $builder->where('union_user.user_type', $requestData['user_type']);
+                !empty($requestData['created_time']) && $builder->whereBetween('union_user.created_time', $requestData['created_time']);
 
-                if ($unionUserQuery !== DB::table('n8_union_users')) {
-                    $unionUser = $unionUserQuery->select('id');
-                    $tableName = $this->model->getTable();
-                    $builder->LeftjoinSub($unionUser, 'union_user', function ($join) use ($tableName,$unionUserId) {
-                        $join->on($tableName . '.' . $unionUserId, '=', 'union_user.id');
-                    });
-                    $builder->selete(DB::raw($tableName.'.*'));
-                }
+
             });
         });
     }
