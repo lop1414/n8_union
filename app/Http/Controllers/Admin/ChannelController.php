73d9a1da354c638902105caff02623a7ea2421e0
linyouxin <yllobx@outlook.com>
2022-03-31 16:05:02 +0800

    -

--- app/Http/Controllers/Admin/ChannelController.php
+++ app/Http/Controllers/Admin/ChannelController.php
@@ -103,7 +103,7 @@ class ChannelController extends BaseController
         $this->curdService->selectQueryAfter(function(){
             $feedbackUrlParam = [];
             if($this->adminUserService->isSupport()){
-                $feedbackUrlParam['support_admin_id'] = $this->adminUserService->isSupport();
+                $feedbackUrlParam['support_admin_id'] = $this->adminUserService->readId();
             }
             $advFeedBack = Advs::getFeedbackUrlMap($feedbackUrlParam);
             $advPageFeedBack = Advs::getPageFeedbackUrlMap();
