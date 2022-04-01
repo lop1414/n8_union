9f3186517176ea129f8c6f8f73e73172cff05c61
linyouxin <yllobx@outlook.com>
2022-04-01 14:57:43 +0800

    -

--- app/Http/Controllers/Admin/ChannelController.php
+++ app/Http/Controllers/Admin/ChannelController.php
@@ -86,7 +86,7 @@ class ChannelController extends BaseController
 
             $keyword = $req['keyword'] ?? '';
             if(!empty($keyword)){
-                $builder->whereRaw(" ( trim(replace(`name`,' ','')) LIKE trim(replace('%{$keyword}%',' ','')) OR `id` LIKE '%{$keyword}%')");
+                $builder->whereRaw(" (`name` LIKE '%{$keyword}%' OR `id` LIKE '%{$keyword}%')");
             }
         });
     }
