d1a76484f4eb881eb27e624986a86269240cfb14
linyouxin <yllobx@outlook.com>
2022-05-30 14:42:46 +0800

    后台BookController 增加keyword搜索

--- app/Http/Controllers/Admin/BookController.php
+++ app/Http/Controllers/Admin/BookController.php
@@ -17,4 +17,35 @@ class BookController extends BaseController
         parent::__construct();
     }
 
+    /**
+     * 过滤
+     */
+    public function dataFilter(){
+
+        $this->curdService->customBuilder(function ($builder){
+            $keyword = $req['keyword'] ?? '';
+            if(!empty($keyword)){
+                $builder->whereRaw(" (`name` LIKE '%{$keyword}%' OR `id` LIKE '%{$keyword}%' OR `cp_book_id` LIKE '%{$keyword}%')");
+            }
+        });
+    }
+
+    /**
+     * 分页列表预处理
+     */
+    public function selectPrepare(){
+        $this->curdService->selectQueryBefore(function (){
+            $this->dataFilter();
+        });
+    }
+
+    /**
+     * 列表预处理
+     */
+    public function getPrepare(){
+        $this->curdService->getQueryBefore(function (){
+            $this->dataFilter();
+        });
+    }
+
 }
