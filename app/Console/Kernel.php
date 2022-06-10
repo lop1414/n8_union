646ebe1408398b9cc096d72b8006c4e791ca803b
linyouxin <yllobx@outlook.com>
2022-06-10 17:27:07 +0800

    -

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Common\Enums\AdvAliasEnum;
+use App\Common\Helpers\Functions;
 use App\Console\Commands\RefreshWeixinMiniProgramAccessTokenCommand;
 use App\Console\Commands\SyncDeviceNetworkLicenseCommand;
 use App\Console\Commands\UaReadAnalyseCommand;
@@ -61,15 +62,32 @@ class Kernel extends ConsoleKernel
         //五分钟区间
         $fiveMinuteRange = "'".date('Y-m-d H:i:s',TIMESTAMP-60*5)."','{$dateTime}'";
 
-        //创建分表
-        $schedule->command('create_table')->cron('0 0 1,15 * *');
+        // 正式
+        if(Functions::isProduction()){
+            //创建分表
+            $schedule->command('create_table')->cron('0 0 1,15 * *');
 
-        //刷新小程序token
-        $schedule->command('refresh_weixin_program_access_token')->cron('0 * * * *');
+            //拉取CP渠道
+            $schedule->command('pull_cp_channel --date=today')->cron('50 23 * * *');
 
-        //拉取CP渠道
-        $schedule->command('pull_cp_channel --date=today')->cron('50 23 * * *');
+            //刷新小程序token
+            $schedule->command('refresh_weixin_program_access_token')->cron('0 * * * *');
 
+            //行为匹配
+            $matchAdv = [AdvAliasEnum::OCEAN,AdvAliasEnum::BD,AdvAliasEnum::KS,AdvAliasEnum::UC,AdvAliasEnum::GDT];
+            $matchAction = ['reg','follow','add_shortcut','order','complete_order'];
+            foreach ($matchAdv as $advAlias){
+                foreach ($matchAction as $action){
+                    $matchCommand = "user_action_match --adv_alias={$advAlias}  --time='{$hourRange24}' --action={$action}";
+                    $schedule->command($matchCommand)->cron('* * * * *');
+                }
+            }
+
+            //同步设备入网许可信息 每月1号执行
+            $year = date('Y');
+            $schedule->command("sync_device_network_license  --year='{$year}'")->cron('* * 1 * *');
+
+        }
 
         //行为数据入库
         $userAction = ['reg','follow','add_shortcut','order','complete_order','login','read'];
@@ -77,22 +95,5 @@ class Kernel extends ConsoleKernel
             $matchCommand = "save_user_action --action={$action}";
             $schedule->command($matchCommand)->cron('* * * * *');
         }
-
-
-        //行为匹配
-        $matchAdv = [AdvAliasEnum::OCEAN,AdvAliasEnum::BD,AdvAliasEnum::KS,AdvAliasEnum::UC,AdvAliasEnum::GDT];
-        $matchAction = ['reg','follow','add_shortcut','order','complete_order'];
-        foreach ($matchAdv as $advAlias){
-            foreach ($matchAction as $action){
-                $matchCommand = "user_action_match --adv_alias={$advAlias}  --time='{$hourRange24}' --action={$action}";
-                $schedule->command($matchCommand)->cron('* * * * *');
-            }
-        }
-
-
-        //同步设备入网许可信息 每月1号执行
-        $year = date('Y');
-        $schedule->command("sync_device_network_license  --year='{$year}'")->cron('* * 1 * *');
-
     }
 }
