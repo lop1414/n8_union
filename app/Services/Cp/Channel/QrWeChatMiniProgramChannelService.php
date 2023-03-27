00bc57e962f42794f08c84f5a48ea31504abe5bb
linyouxin <yllobx@outlook.com>
2023-03-27 10:40:24 +0800

    -

--- app/Services/Cp/Channel/QrWeChatMiniProgramChannelService.php
+++ app/Services/Cp/Channel/QrWeChatMiniProgramChannelService.php
@@ -6,6 +6,7 @@ namespace App\Services\Cp\Channel;
 use App\Common\Enums\CpTypeEnums;
 use App\Common\Enums\ProductTypeEnums;
 use App\Common\Sdks\Qr\QrSdk;
+use App\Common\Tools\CustomException;
 use App\Services\BookService;
 use App\Services\ChapterService;
 
@@ -39,40 +40,52 @@ class QrWeChatMiniProgramChannelService implements CpChannelInterface
 
         $startTime = $date.' 00:00:00';
         $endTime = $date.' 23:59:59';
-        $channels = $sdk->getChannelList($startTime,$endTime);
 
-        if($channels['total'] <= 0){
-            return [];
+        try {
+            $channels = $sdk->getChannelList($startTime,$endTime);
+
+            if($channels['total'] <= 0){
+                return [];
+            }
+
+            foreach ($channels['promotionList'] as $item){
+                // 书籍
+                $book['id'] = 0;
+
+                $book = $bookService->readSave([
+                    'cp_book_id' => $item['video_id'],
+                    'name'       => $item['video_name'],
+                    'cp_type'    => $product['cp_type']
+                ]);
+
+                // 打开章节
+                $chapter = $chapterService->readSave($book['id'],$item['chapter_id'],$item['chapter_title'],$item['chapter_order']);
+
+
+                $data[] = [
+                    'product_id'    => $product['id'],
+                    'cp_channel_id' => $item['promotion_id'],
+                    'name'          => $item['promotion_name'],
+                    'book_id'       => $book['id'],
+                    'chapter_id'    => $chapter['id'] ?? 0,
+                    'force_chapter_id' => $forceChapter['id'] ?? 0,
+                    'extends'       => [
+                        'h5_url'    => $item['promotion_wechat_redirect_url'],
+                    ],
+                    'create_time' => $item['create_time'],
+                    'updated_time' => $item['create_time'],
+                ];
+            }
+
+        } catch (CustomException $e) {
+            $errInfo = $e->getErrorInfo(true);
+            // 请求接口过频 异常过滤
+            if($errInfo['data']['result']['code'] != '30001'){
+                throw $e;
+            }
         }
 
-        foreach ($channels['promotionList'] as $item){
-            // 书籍
-            $book['id'] = 0;
 
-            $book = $bookService->readSave([
-                'cp_book_id' => $item['video_id'],
-                'name'       => $item['video_name'],
-                'cp_type'    => $product['cp_type']
-            ]);
-
-            // 打开章节
-            $chapter = $chapterService->readSave($book['id'],$item['chapter_id'],$item['chapter_title'],$item['chapter_order']);
-
-
-            $data[] = [
-                'product_id'    => $product['id'],
-                'cp_channel_id' => $item['promotion_id'],
-                'name'          => $item['promotion_name'],
-                'book_id'       => $book['id'],
-                'chapter_id'    => $chapter['id'] ?? 0,
-                'force_chapter_id' => $forceChapter['id'] ?? 0,
-                'extends'       => [
-                    'h5_url'    => $item['promotion_wechat_redirect_url'],
-                ],
-                'create_time' => $item['create_time'],
-                'updated_time' => $item['create_time'],
-            ];
-        }
 
         return $data;
     }
